494e924947cde474dbb32186a6c7e3f6bba019f6
Marko Mazibrada <biomazi@gmail.com>
2022-06-03 09:52:54 +0200

    MR test

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Small Change to merge
         </a>
       </header>
     </div>
